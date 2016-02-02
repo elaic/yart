fdacd67581c98cd91a32a7d2b7a42fa024fdbf16
Marcin Lysniewski <marcin.lysniewski@gmail.com>
2016-02-02 20:39:04 +0100

    Added tinyobjloader for asset loading

---
+++ external/tinyobjloader/tiny_obj_loader.cc
@@ -0,0 +1,8 @@
+#define TINYOBJLOADER_IMPLEMENTATION
+#if defined(_MSC_VER)
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#pragma warning(push)
+#pragma warning(disable : 4706)
+#include "tiny_obj_loader.h"
+#pragma warning(pop)
