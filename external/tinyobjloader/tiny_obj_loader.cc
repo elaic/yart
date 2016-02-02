01a7a2ef5fcbfb8ce40488f5c7f159b5f7c9848e
Marcin Lysniewski <marcin.lysniewski@gmail.com>
2016-02-02 20:42:48 +0100

    gcc build fix for previous commit

--- external/tinyobjloader/tiny_obj_loader.cc
+++ external/tinyobjloader/tiny_obj_loader.cc
@@ -1,8 +1,13 @@
 #define TINYOBJLOADER_IMPLEMENTATION
+
 #if defined(_MSC_VER)
 #define _CRT_SECURE_NO_WARNINGS
-#endif
 #pragma warning(push)
 #pragma warning(disable : 4706)
+#endif
+
 #include "tiny_obj_loader.h"
+
+#if defined(_MSC_VER)
 #pragma warning(pop)
+#endif
